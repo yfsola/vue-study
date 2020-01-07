6a99ec0d311f5f9592995ffe94245e268eb76293
yfaola <m15500104512@163.com>
2020-01-07 08:23:16 +0800

    test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,6 +8,7 @@
     <p @click="$store.commit('add')">counter:{{$store.state.counter}}</p>
     <p @click="$store.dispatch('add')">async counter:{{$store.state.counter}}</p>
     <p>double counter:{{$store.getters.doubleCounter}}</p>
+    123
   </div>
 </template>
 
