e82b12aa82f28bd53804be02364bd4fbf19166f3
solayf <m15500104512@163.com>
2020-03-17 22:48:32 +0800

    aa

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
     <Hello></Hello>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
-// import Hello from './components/Hello'
+// import HelloWorld from './components/HelloWorld.vue';
+import Hello from './components/Hello'
 
 @Component({
   components: {
-    HelloWorld,
-    // Hello
+    // HelloWorld,
+    Hello
   },
 })
 export default class App extends Vue {}
--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,18 +1,19 @@
 
 <template>
   <div>
+    SHU
     <ul>
       <li v-for="item in features" :key="item">{{item}}</li>
     </ul>
   </div>
 </template>
 <script lang="ts">
-  import { Component, Prop, Vue } from "vue-property-decorator";
+  import { Component, Vue } from "vue-property-decorator";
 
   @Component
-  export default Hello extends Vue {
+  export default class Hello extends Vue {
     //属性就是data
-    // features: string[] = ['类型注解', '编译型语言']
+    features: string[] = ['类型注解', '编译型语言']
   }
 
 </script>
