98df98215fcd4adac1482ba74b69c41de21410ec
solayf <m15500104512@163.com>
2020-03-17 22:40:24 +0800

    update

---
+++ src/components/Hello.vue
@@ -0,0 +1,18 @@
+
+<template>
+  <div>
+    <ul>
+      <li v-for="item in features" :key="item">{{item}}</li>
+    </ul>
+  </div>
+</template>
+<script lang="ts">
+  import { Component, Prop, Vue } from "vue-property-decorator";
+
+  @Component
+  export default Hello extends Vue {
+    //属性就是data
+    // features: string[] = ['类型注解', '编译型语言']
+  }
+
+</script>
